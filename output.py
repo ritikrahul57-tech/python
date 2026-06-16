f22dcebb5f714c64c088ee15ae2371127af17fed
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-16 22:00:21 +0530

    The python programms

---
+++ output.py
@@ -0,0 +1,33 @@
+x="python"
+y="is"
+z="awesome"
+print(x,y,z)
+
+
+x='python'
+y='is'
+z='awesome'
+print(x+y+z)
+
+
+x=7
+z=2
+print(x+z)
+
+
+x=6
+z='rahul'
+print(x+z)  #this one will be a error
+
+
+x=6
+z='rahul'
+print(x,z)
+
+
+x='easy one'
+def myfunc():
+    print('python is'+x)
+myfunc()
+
+
